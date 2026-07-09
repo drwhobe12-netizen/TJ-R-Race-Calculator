3cd71dab4b3d272e7262099b84f74f789ba83709
drwhobe12-netizen <drwhobe12@gmail.com>
2026-07-09 09:24:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thrapston-race-planner-v1';
+const CACHE_NAME = 'thrapston-race-planner-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
